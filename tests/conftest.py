ff328debd0dd04cfc6975a81363f0b91e8774aad
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2025-10-04 11:12:32 -0700

    Add support for AWS provider version 6.*

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,6 @@ import logging
 
 from infrahouse_core.logging import setup_logging
 
-# TEST_ROLE_ARN = "arn:aws:iam::303467602807:role/gh-identity-provider-tester"
 DEFAULT_PROGRESS_INTERVAL = 10
 TERRAFORM_ROOT_DIR = "test_data"
 
